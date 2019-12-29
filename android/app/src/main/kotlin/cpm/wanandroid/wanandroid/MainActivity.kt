10daba1000a29bd2d9147779cca15826cf03cb1c
程孟辉 <cheng_oop@foxmail.com>
2019-12-29 16:34:32 +0800

    first commit

---
+++ android/app/src/main/kotlin/cpm/wanandroid/wanandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package cpm.wanandroid.wanandroid
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
